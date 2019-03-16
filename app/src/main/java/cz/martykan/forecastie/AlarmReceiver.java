e2414f3034ad191fda47cfb3a2186ffa50efc46c
Eduardo Rocha <hockdudu@yandex.com>
2019-03-16 17:24:42 +0100

    Fix wrong city being displayed in the widget

--- app/src/main/java/cz/martykan/forecastie/AlarmReceiver.java
+++ app/src/main/java/cz/martykan/forecastie/AlarmReceiver.java
@@ -104,7 +104,7 @@ public class AlarmReceiver extends BroadcastReceiver {
                 String language = Locale.getDefault().getLanguage();
                 if(language.equals("cs")) { language = "cz"; }
                 String apiKey = sp.getString("apiKey", context.getResources().getString(R.string.apiKey));
-                URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=" + URLEncoder.encode(sp.getString("city", Constants.DEFAULT_CITY), "UTF-8") + "&lang="+ language +"&appid=" + apiKey);
+                URL url = new URL("https://api.openweathermap.org/data/2.5/weather?id=" + URLEncoder.encode(sp.getString("cityId", Constants.DEFAULT_CITY_ID), "UTF-8") + "&lang="+ language +"&appid=" + apiKey);
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 BufferedReader r = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
 
@@ -148,7 +148,7 @@ public class AlarmReceiver extends BroadcastReceiver {
                 String language = Locale.getDefault().getLanguage();
                 if(language.equals("cs")) { language = "cz"; }
                 String apiKey = sp.getString("apiKey", context.getResources().getString(R.string.apiKey));
-                URL url = new URL("https://api.openweathermap.org/data/2.5/forecast?q=" + URLEncoder.encode(sp.getString("city", Constants.DEFAULT_CITY), "UTF-8") + "&lang="+ language +"&mode=json&appid=" + apiKey);
+                URL url = new URL("https://api.openweathermap.org/data/2.5/forecast?id=" + URLEncoder.encode(sp.getString("cityId", Constants.DEFAULT_CITY_ID), "UTF-8") + "&lang="+ language +"&mode=json&appid=" + apiKey);
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 BufferedReader r = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
 
@@ -298,18 +298,12 @@ public class AlarmReceiver extends BroadcastReceiver {
                     Log.d(TAG, "JSON Result: " + result);
                     try {
                         JSONObject reader = new JSONObject(result);
-                        String city = reader.getString("name");
-                        String country = "";
-                        JSONObject countryObj = reader.optJSONObject("sys");
-                        if (countryObj != null) {
-                            country = ", " + countryObj.getString("country");
-                        }
-                        Log.d(TAG, "City: " + city + country);
-                        String lastCity = PreferenceManager.getDefaultSharedPreferences(context).getString("city", "");
-                        String currentCity = city + country;
+                        String cityId = reader.getString("id");
+                        Log.d(TAG, "City ID: " + cityId);
+                        String lastCity = PreferenceManager.getDefaultSharedPreferences(context).getString("cityId", "");
                         SharedPreferences.Editor editor = sp.edit();
-                        editor.putString("city", currentCity);
-                        editor.putBoolean("cityChanged", !currentCity.equals(lastCity));
+                        editor.putString("cityId", cityId);
+                        editor.putBoolean("cityChanged", !cityId.equals(lastCity));
                         editor.commit();
 
                     } catch (JSONException e){
